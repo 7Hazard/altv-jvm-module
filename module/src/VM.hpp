a14946c621f14e6c8c37b2fe23abb93eba690435
Hazard <hazard9909@gmail.com>
2020-07-08 02:36:48 +0200

    Dynamic loading of JVM and JNI

--- module/src/VM.hpp
+++ module/src/VM.hpp
@@ -5,26 +5,62 @@
 #include <filesystem>
 #include "Util.hpp"
 
+#define JAR_NAME "altv-jvm-module.jar"
+#define JAR_RELATIVE_PATH "modules/altv-jvm-module/" JAR_NAME
+#define JAR_MAIN_CLASS "hazard7/altv/jvm/Main"
+
 class VM
 {
 public:
 
-    static JavaVM*& jvm()
+    HMODULE jvmlib = nullptr;
+    void LoadLib()
     {
-        static JavaVM* jvm;
-        return jvm;
+        jvmlib = LoadLibrary(L"jvm");
+        if(!jvmlib)
+        {
+            util::loge("[JVM] Could not load jvm.dll");
+        }
     }
 
-    static bool Start(alt_ICore* core)
+    jint (JNICALL *CreateJavaVM)(JavaVM **pvm, void **penv, void *args) = nullptr;
+    
+    uint8_t failedFuncs = 0;
+    void* LoadFunc(const std::string& name)
+    {
+        auto addr = (void*)GetProcAddress(jvmlib, name.c_str());
+        if(!addr)
+        {
+            util::loge("Could not load " + name + " from jvm.dll");
+            failedFuncs++;
+        }
+        return addr;
+    }
+
+    JavaVM* jvm = nullptr;
+
+    bool Start()
     {
         // Checks
-        // if(!util::FileExists(core, "modules/altv-jvm-module/" JVM_JAR_NAME))
-        // {
-        //     util::loge(core, "[JVM] Could not find file 'modules/altv-jvm-module/" JVM_JAR_NAME "'");
-        //     return false;
-        // }
+        if(!std::filesystem::exists(JAR_RELATIVE_PATH))
+        {
+            util::loge("[JVM] Could not find file '" JAR_RELATIVE_PATH "'");
+            return false;
+        }
 
-        util::logi(core, "[JVM] Starting Java VM (Module Version '" JVM_MODULE_VERSION "')");
+        // Dynamic load jvm
+        LoadLib();
+        if(jvmlib == nullptr) return false;
+
+        // dynamic load jni funcs
+        CreateJavaVM = (decltype(CreateJavaVM))LoadFunc("JNI_CreateJavaVM");
+        if(failedFuncs > 0)
+        {
+            // util::loge("[JVM] Could not properly load all JNI functions");
+            return false;
+        }
+
+        util::logi("[JVM] Starting Java VM (Module Version '" JVM_MODULE_VERSION "')");
 
         JNIEnv *env; /* pointer to native method interface */
         JavaVMInitArgs vm_args; /* JDK/JRE 6 VM initialization arguments */
@@ -36,7 +72,7 @@ public:
         vm_args.nOptions = 5;
         vm_args.options = options;
         // OBS: SYNTAX MISTAKES CAN CAUSE INSTA-CRASH
-        options[0].optionString = "-Djava.class.path=modules/altv-jvm-module;modules/altv-jvm-module/" JVM_JAR_NAME;
+        options[0].optionString = "-Djava.class.path=modules/altv-jvm-module;" JAR_RELATIVE_PATH;
         options[1].optionString = "-Djava.library.path=modules/altv-jvm-module";
         options[2].optionString = "-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005";
         options[3].optionString = "-XX:+ShowMessageBoxOnError";
@@ -46,28 +82,38 @@ public:
         vm_args.version = JNI_VERSION_1_6;
         vm_args.ignoreUnrecognized = JNI_TRUE;
 
-        jint vmres = JNI_CreateJavaVM(&jvm(), (void**)&env, &vm_args);
+        jint vmres = CreateJavaVM(&jvm, (void**)&env, &vm_args);
         if(vmres < 0)
         {
-            util::loge(core, "[JVM] Could not start JVM\n\t\t Error code: "+std::to_string(vmres));
+            util::loge("[JVM] Could not start JVM\n\t\t Error code: "+std::to_string(vmres));
+            return false;
+        }
+        
+        vmres = jvm->AttachCurrentThread((void**)&env, nullptr);
+        if(vmres < 0)
+        {
+            util::loge("[JVM] Could not Attach JVM to thread\n\t\t Error code: "+std::to_string(vmres));
             return false;
         }
 
-        jclass cls = env->FindClass("hazard7/altv/jvm/Main");
+        // std::string vmver = "[JVM] Started Java VM version " + std::to_string(env->GetVersion());
+        // util::logi(vmver);
+        
+        jclass cls = env->FindClass(JAR_MAIN_CLASS);
         if(cls == nullptr)
         {
-            util::loge(core, "[JVM] " JVM_JAR_NAME " is corrupt or doesn't exist");
+            util::loge("[JVM] " JAR_NAME " is corrupted: could not load class " JAR_MAIN_CLASS);
             return false;
         }
         jmethodID mid = env->GetStaticMethodID(cls, "main", "(JZ)V");
         if(mid == nullptr)
         {
-            util::loge(core, "[JVM] Could not find 'void Main::main(long, boolean)'");
+            util::loge("[JVM] " JAR_NAME " is corrupted: could not find 'void Main::main(long, boolean)' in " JAR_MAIN_CLASS);
             return false;
         }
 
 #ifdef _DEBUG
-        util::logi(core, "[JVM] Debug Build");
+        util::logi("[JVM] Debug Build");
         env->CallStaticVoidMethod(cls, mid, core, true);
 #else
         env->CallStaticVoidMethod(cls, mid, core, false);
@@ -76,7 +122,7 @@ public:
         jboolean flag = env->ExceptionCheck();
         if (flag)
         {
-            util::loge(core, "Exception occurred while executing Java entry point");
+            util::loge("[JVM] Exception occurred while executing Java entry point");
             env->ExceptionDescribe();
             env->ExceptionClear();
         }
@@ -84,8 +130,8 @@ public:
         return true;
     }
 
-    static void Dispose()
+    void Dispose()
     {
-        jvm()->DestroyJavaVM();
+        jvm->DestroyJavaVM();
     }
 };
